f245e8a5e3759576ad75a0fad856314fe3017681
jianfeng.lu <269219362@qq.com>
2018-12-26 16:54:46 +0800

    add test service

---
+++ src/test/java/com/example/demo/GuavaTest.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import com.google.common.collect.Sets;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class GuavaTest {
+
+    @Test
+    public void setTest(){
+        Set<Integer> oldset=new HashSet<>();
+        oldset.addAll(Arrays.asList(1,2,3,4,256));
+        Set<Integer> newset=new HashSet<>();
+        newset.addAll(Arrays.asList(1,2,256,5,6,7));
+        Set diff= Sets.difference(oldset,newset);
+        diff.stream().forEach(System.out::println);
+    }
+
+
+
+
+
+
+
+
+
+
+}
